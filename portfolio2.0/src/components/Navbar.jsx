2bf2c7c1a0c84418176aa8872217007e9a4bea33
Evan St Martin <mantisTMD1@gmail.com>
2022-12-01 17:56:38 -0600

    wrote a garbage hook to close nav on click, doesnt work

--- portfolio2.0/src/components/Navbar.jsx
+++ portfolio2.0/src/components/Navbar.jsx
@@ -1,27 +1,58 @@
 import React from "react";
 import { stack as Menu } from "react-burger-menu";
 import "../styles/navbar.css";
+import { useRef } from "react";
 
 const NavMenu = (props) => {
+  const offNavRef = useRef();
+  const offsetValue = -280;
+
+  const closeOffCanvas = () => offNavRef.current.backdrop.click();
+
   return (
-    <Menu {...props} right width={"280px"}>
-      <a className="menu-item" href="#home">
+    <Menu right width={"280px"} ref={offNavRef}>
+      <a
+        className="menu-item"
+        href="#home"
+        onClick={closeOffCanvas}
+        offset={offsetValue}
+      >
         Home
       </a>
 
-      <a className="menu-item" href="#about ">
+      <a
+        className="menu-item"
+        href="#about"
+        onClick={closeOffCanvas}
+        offset={offsetValue}
+      >
         <span>00.</span>About
       </a>
 
-      <a className="menu-item" href="#tech">
+      <a
+        className="menu-item"
+        href="#tech"
+        onClick={closeOffCanvas}
+        offset={offsetValue}
+      >
         <span>01.</span>Tech
       </a>
 
-      <a className="menu-item" href="#projects">
+      <a
+        className="menu-item"
+        href="#projects"
+        onClick={closeOffCanvas}
+        offset={offsetValue}
+      >
         <span>02.</span>Projects
       </a>
 
-      <a className="menu-item" href="#contact">
+      <a
+        className="menu-item"
+        href="#contact"
+        onClick={closeOffCanvas}
+        offset={offsetValue}
+      >
         <span>03.</span>Contact
       </a>
     </Menu>
